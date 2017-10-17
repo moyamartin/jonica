bea5364ee5c3f3fbd95c4edff40fd859d9b1f4d7
Leandro Soria <leandrosoria@hotmail.com>
2017-10-17 20:41:46 -0300

    Todos los archivos

--- audio2.py
+++ audio2.py
@@ -18,19 +18,33 @@ DCNeutral = (DC180+DC0)/2
 m = (DC180-DC0)/180.0
 h = DC0
 ##################
-UMBRAL = 5.0
+Flag_newValue = 0
+UMBRAL_MIN = 1.0
+UMBRAL_MAX = 30.0
+
+Outliers_cnt = 0
+Outlierts_MAX = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+pwm = GPIO.PWM(24, FServo)
+# Servo en el canal 24
+pwm.start(DCNeutral)
+
 
 # Obtener el DC a partir del angulo
 def set_angle(angle):
-	global m
-	global h
-	return (m*angle + h)
+    global m
+    global h
+    global pwm
+    setpoint = (m*angle + h)                                    
+    if setpoint < 0.0 and setpoint > 100.0:
+        print("DC invalido")
+    else:
+        print("Setpoint dCycle: %.2f" % setpoint)
+        #Cambio el dCycle del servo
+        pwm.ChangeDutyCycle(setpoint)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24,GPIO.OUT)
-pwm = GPIO.PWM(24,FServo)
-# Servo en el canal 24
-pwm.start(DCNeutral)
 # Servo comienza en posicion central 
 theta_sa = 90.0 #angulo actual del servo referido al sistema de referencia absoluto
 theta_sf = 0.0
@@ -76,28 +90,31 @@ while True:
         theta_m = 180.0 - (180.0/np.pi)*np.arccos((c*delay)/(Fs*d) )   
     if np.isnan(theta_m):
         print ("Ignorado")
-    elif (theta_m > 180.0):
-        print ("Theta > 180")
+    elif (theta_m > 180.0 or theta_m < 0.0):
+        print ("Angulo medido fuera de rango permitido por el servo")
     else:
-        print("Indice: ", index)
+        print("Indice: %d" % index)
         print("Angulo Estimado: %.2f" % theta_m)
          # Calculo el angulo final del servo (donde debe ir) a partir del angulo actual y del medido por los mics
         theta_sf = theta_m + theta_sa - 90
+        print("Angulo Actual: %.2f" % theta_sa)
+        print("Nuevo Angulo: %.2f" % theta_sf)
          # Obtengo el dCycle para el servo a partir del angulo
-        if(np.abs(theta_sf-theta_sa) < UMBRAL ):
+        if(np.abs(theta_sf-theta_sa) < UMBRAL_MIN ):
             print("Umbral chico, no muevo el servo")
+        elif (Flag_newValue == 0):          # Flag en 0, quiere decir que es una nueva medicion
+            set_angle(theta_sf)             # Seteo el angulo calculado
+            theta_sa = theta_sf
+            Flag_newValue = 1               # Seteo el flag
         else:
-            setpoint = set_angle(theta_sf)                                     
-            if (setpoint < 0.0 and setpoint > 100.0):
-                print("DC invalido")
-            else:
-                print("Setpoint dCycle: %.2f" % setpoint)
-                #Cambio el dCycle del servo
-                pwm.ChangeDutyCycle(setpoint)                                 
-        
-    # Actualizo el valor de angulo actual para la proxima iteracion
-    theta_sa = theta_sf                                                
-
+            # Si entro aca quiere decir que ya se realizo una medicion antes, tengo que ver si hay mucha diferencia en grados
+            if (np.abs(theta_sf-theta_sa) > UMBRAL_MAX ):
+                print("Posible Oulier, espero un poco... (intento %d de %d)" % (Outliers_cnt, Outlierts_MAX) ) 
+                Outliers_cnt = Outliers_cnt+1
+                if (Outliers_cnt == Outlierts_MAX):
+                    print("Demasiados outliers, debe ser un nuevo valor. Reinicio Flag...")
+                    Outliers_cnt = 0
+                    Flag_newValue = 0   # Al bajar el flag, en la proxima iteracion cae al statement elif anterior                                             
 
 pwm.stop()
 GPIO.cleanup()
